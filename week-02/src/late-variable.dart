fd4fd22327d995cb42de5124a42ee6936197f5ac
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-04 09:54:40 +0700

    update

---
+++ week-02/src/late-variable.dart
@@ -0,0 +1,12 @@
+void main() {
+  late String name;
+  // Cetak nilai variabel late
+  // Akan menghasilkan error runtime
+  // print(name);
+
+  // Set nilai variabel late
+  name = "John Doe";
+
+  // Cetak nilai variabel late
+  print(name); // Output: John Doe
+}
