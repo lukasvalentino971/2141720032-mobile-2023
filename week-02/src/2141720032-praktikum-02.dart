e1e41b44a2797d5f524492e1c35c15647248c61a
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-04 11:18:48 +0700

    update jawaban

--- week-02/src/2141720032-praktikum-02.dart
+++ week-02/src/2141720032-praktikum-02.dart
@@ -1,6 +1,5 @@
 void main() {
   for (int i = 18; i >= 9; i--) {
-    String formattedAge = i < 10 ? '0$i' : '$i';
-    print('Nama saya adalah Fulan, sekarang berumur $formattedAge');
+    print('Nama saya adalah Fulan, sekarang berumur $i');
   }
 }
