9a3e190c934d88978013f6a31ef57b35f0225286
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-11 10:03:38 +0700

    update

--- week-03/src/praktikum3.dart
+++ week-03/src/praktikum3.dart
@@ -1,5 +1,14 @@
 void main(){
-  for (int Index = 10; Index < 27; Index++) {
+  // for (int Index = 10; Index < 27; Index++) {
+  //   print(Index);
+  // }
+//   If (Index == 21) break;
+// Else If (index > 1 || index < 7) continue;
+// print(index);
+
+  for (int Index = 1; Index <= 27; Index++) {
+    if (Index == 21) break;
+    else if (Index > 1 && Index < 7) continue;
     print(Index);
   }
 }
