60269f672538d3a8f7ef4b40d64bd1d3e7fa3274
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-11 17:13:53 +0700

    update

--- week-03/src/TugasPraktikum-03.dart
+++ week-03/src/TugasPraktikum-03.dart
@@ -1,30 +1,33 @@
-void main(List<String> args) {
-  findPrimeNumber();
-}
+//Tugas
+List<int> sieveAlgoritm(int n) {
+  List<bool> isPrime = List<bool>.filled(n + 1, true);
+  List<int> primes = [];
 
-void findPrimeNumber() {
-  int start = 0;
-  int n = 201;
-  String nama = "Maulana Arif Wijaya";
-  String nim = "2141720085";
-
-  for (int i = start; i <= n; i++) {
-    if (i == 0 || i == 1) {
-      continue;
-    }
-
-    bool isPrime = true;
-
-    for (int j = 2; j <= i / 2; j++) {
-      if (i % j == 0) {
-        isPrime = false;
-        break;
+  for (int p = 2; p * p <= n; p++) {
+    if (isPrime[p]) {
+      for (int i = p * p; i <= n; i += p) {
+        isPrime[i] = false;
       }
     }
+  }
 
-    if (isPrime) {
-      print("Nama: $nama \nNIM: $nim");
-      print("bilangan prima: $i");
+  for (int i = 2; i <= n; i++) {
+    if (isPrime[i]) {
+      primes.add(i);
     }
   }
+
+  return primes;
+}
+
+void main() {
+  int n = 201; 
+  String nama = "Lukas Valentino";
+  String nim = "2141720032";
+  List<int> primeNumbers = sieveAlgoritm(n);
+
+  print("Bilangan prima dari 2 hingga $n adalah:");
+  print(primeNumbers);
+  print("Nama : $nama");
+  print("NIM : $nim");
 }
