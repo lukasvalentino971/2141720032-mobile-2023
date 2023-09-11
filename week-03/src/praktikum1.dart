437edf414d3feca88957eda04943ee9227182028
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-11 10:21:58 +0700

    update

--- week-03/src/praktikum1.dart
+++ week-03/src/praktikum1.dart
@@ -1,14 +1,26 @@
 void main() {
-  String test = "test2";
-  if (test == "test1") {
-    print("Test1");
-  } else if (test == "test2") {
-    print("Test2");
-  } else {
-    print("Something else");
+  // String test = "test2";
+  // if (test == "test1") {
+  //   print("Test1");
+  // } else if (test == "test2") {
+  //   print("Test2");
+  // } else {
+  //   print("Something else");
+  // }
+
+  // if (test == "test2") {
+  //   print("Test2 again");
+  // }
+
+//   String test = "true";
+// if (test) {
+//    print("Kebenaran");
+// }
+
+
+  bool test = true; // Menggunakan boolean true
+  if (test) {
+    print("Kebenaran");
   }
 
-  if (test == "test2") {
-    print("Test2 again");
-  }
 }
