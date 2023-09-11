0ea1f622e1cd47a182da02ff94ef86a1f4ea9aaf
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-11 09:39:46 +0700

    update

--- week-03/src/praktikum2.dart
+++ week-03/src/praktikum2.dart
@@ -1,7 +1,14 @@
 void main(){
   int counter = 0; // Inisialisasi variabel counter
-  while (counter < 33) {
-    print(counter);
-    counter++;
-  }
+  // langkah 2
+  // while (counter < 33) {
+  //   print(counter);
+  //   counter++;
+  // }
+
+  //langkah3
+  do {
+  print(counter);
+  counter++;
+} while (counter < 77);
 }
