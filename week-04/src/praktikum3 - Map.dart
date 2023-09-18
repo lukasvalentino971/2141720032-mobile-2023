9775ba809bccc99c91da46ab84ccfa6ca7a2683a
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-18 10:19:03 +0700

    update

---
+++ week-04/src/praktikum3 - Map.dart
@@ -0,0 +1,17 @@
+void main() {
+  var gifts = {
+  // Key:    Value
+  'first': 'partridge',
+  'second': 'turtledoves',
+  'fifth': 1
+};
+
+var nobleGases = {
+  2: 'helium',
+  10: 'neon',
+  18: 2,
+};
+
+print(gifts);
+print(nobleGases);
+}
