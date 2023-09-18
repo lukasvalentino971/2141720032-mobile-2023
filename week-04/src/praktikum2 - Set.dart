c204fdb8a9dfe6cf2a6a0ce019dc79e8b0981a7d
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-18 10:01:59 +0700

    update

--- week-04/src/praktikum2 - Set.dart
+++ week-04/src/praktikum2 - Set.dart
@@ -1,4 +1,24 @@
 void main() {
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-  print(halogens);
+  // var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  // print(halogens);
+
+  // var names1 = <String>{};
+  // Set<String> names2 = {}; // This works, too.
+  // var names3 = {}; // Creates a map, not a set.
+
+  // print(names1);
+  // print(names2);
+  // print(names3);
+
+
+  var names1 = <String>{};
+  Set<String> names2 = {};
+
+  names1.add("Lukas Valentino");
+  names1.add("2141720032");
+
+  names2.addAll({"Lukas Valentino", "2141720032"});
+
+  print(names1);
+  print(names2);
 }
