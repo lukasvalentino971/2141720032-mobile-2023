b7fdcfd382b3efce61ed907b7fb3b8cd6b424df5
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-18 16:43:09 +0700

    update

--- week-04/src/praktikum4 - Spread&Control FLow.dart
+++ week-04/src/praktikum4 - Spread&Control FLow.dart
@@ -1,3 +1,15 @@
 void main() {
-  
+  // var list = [1, 2, 3];
+  // var list2 = [0, ...list];
+  // print(list1);
+  // print(list2);
+  // print(list2.length);
+
+  void main() {
+  var list = [1, 2, 3];
+  var list2 = [0, ...list];
+  print(list);
+  print(list2);
+  print(list2.length);
+}
 }
