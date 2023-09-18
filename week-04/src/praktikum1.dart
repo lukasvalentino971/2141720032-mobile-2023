3af9689bfd558fc8d3b819c376e9154f20f65f41
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-18 09:37:59 +0700

    update prak 1 step 3

--- week-04/src/praktikum1.dart
+++ week-04/src/praktikum1.dart
@@ -1,11 +1,9 @@
 void main() {
-  var list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
-  print(list.length);
-  print(list[1]);
+  List<String?> finalList = List.filled(6, null); // Buat list dengan panjang 6 dan isi dengan null
+  finalList[1] = "Lukas Valentino"; // Isi elemen indeks ke-1 dengan nama
+  finalList[2] = "2141720032"; // Isi elemen indeks ke-2 dengan NIM
 
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+  print(finalList.length); // Cetak panjang list (6)
+  print(finalList[1]); // Cetak elemen indeks ke-1 (Lukas Valentino)
+  print(finalList[2]); // Cetak elemen indeks ke-2 (2141720032)
 }
