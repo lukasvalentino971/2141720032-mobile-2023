3b0264c2104664cdef2bc757cc9c1d16bd990168
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-18 11:03:57 +0700

    update

--- week-04/src/praktikum3 - Map.dart
+++ week-04/src/praktikum3 - Map.dart
@@ -1,17 +1,62 @@
 void main() {
+//   var gifts = {
+//   // Key:    Value
+//   'first': 'partridge',
+//   'second': 'turtledoves',
+//   'fifth': 1
+// };
+
+// var nobleGases = {
+//   2: 'helium',
+//   10: 'neon',
+//   18: 2,
+// };
+
+// print(gifts);
+// print(nobleGases);
+
+  // var mhs1 = Map<String, String>();
+  // gifts['first'] = 'partridge';
+  // gifts['second'] = 'turtledoves';
+  // gifts['fifth'] = 'golden rings';
+
+  // var mhs2 = Map<int, String>();
+  // nobleGases[2] = 'helium';
+  // nobleGases[10] = 'neon';
+  // nobleGases[18] = 'argon';
+
+
   var gifts = {
-  // Key:    Value
-  'first': 'partridge',
-  'second': 'turtledoves',
-  'fifth': 1
-};
+    // Key:    Value
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 1
+  };
 
-var nobleGases = {
-  2: 'helium',
-  10: 'neon',
-  18: 2,
-};
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 2,
+  };
 
-print(gifts);
-print(nobleGases);
+  var mhs1 = Map<String, String>();
+  gifts['first'] = 'partridge';
+  gifts['second'] = 'turtledoves';
+  gifts['fifth'] = 'golden rings';
+
+  var mhs2 = Map<int, String>();
+  nobleGases[2] = 'helium';
+  nobleGases[10] = 'neon';
+  nobleGases[18] = 'argon';
+
+  mhs1['nama'] = 'Lukas Valentino';
+  mhs1['nim'] = '2141720032';
+
+  mhs2[1] = 'Lukas Valentino';
+  mhs2[2] = '2141720032';
+
+  print(gifts);
+  print(nobleGases);
+  print(mhs1);
+  print(mhs2);
 }
