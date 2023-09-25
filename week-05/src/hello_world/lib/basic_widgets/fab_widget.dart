e864c98e0b98a8c8516a694f30927875422a969c
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-25 15:04:08 +0700

    update

---
+++ week-05/src/hello_world/lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyFabWidget extends StatelessWidget {
+  const MyFabWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          backgroundColor: Colors.pink,
+          child: const Icon(Icons.thumb_up),
+        ),
+      ),
+    );
+  }
+}
