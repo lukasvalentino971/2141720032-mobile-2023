2b0eedc636a662b6d9de93cff605cf34b41d1831
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-09-25 21:56:01 +0700

    update

--- week-05/src/hello_world/lib/main.dart
+++ week-05/src/hello_world/lib/main.dart
@@ -51,9 +51,7 @@ class MyApp extends StatelessWidget {
 //         child: Column(
 //           mainAxisAlignment: MainAxisAlignment.center,
 //           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
+//             const ,
 //             Text(
 //               '$_counter',
 //               style: Theme.of(context).textTheme.headlineMedium,
